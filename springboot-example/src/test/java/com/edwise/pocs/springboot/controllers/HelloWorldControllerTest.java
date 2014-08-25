31c59d93bd3b2b6836ee53d279e53c1e85964958
eanton <eduardo.anton@medianet.es>
2014-08-25 11:56:21 +0200

    Añadido test para el controller. Añadidas para ello en el pom las dependencias necesarias.

---
+++ springboot-example/src/test/java/com/edwise/pocs/springboot/controllers/HelloWorldControllerTest.java
@@ -0,0 +1,26 @@
+package com.edwise.pocs.springboot.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+public class HelloWorldControllerTest {
+
+    private HelloWorldController helloWorldController;
+
+    @Before
+    public void setUp() {
+        helloWorldController = new HelloWorldController();
+    }
+
+    @Test
+    public void testSayHelloWorld() {
+        String msgResult = helloWorldController.sayHelloWorld();
+
+        assertNotNull(msgResult);
+        assertThat(msgResult, is("Hello World in your SpringBoot Application!"));
+    }
+}
