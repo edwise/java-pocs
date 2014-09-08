91ae3849bf39a0dbc2db3f82ed2637c147530a39
eanton <eduardo.anton@medianet.es>
2014-09-08 15:16:37 +0200

    Añadido test para la aplicación Spring Boot.

---
+++ spring-rest-example/src/test/java/com/edwise/pocs/springrest/ApplicationTest.java
@@ -0,0 +1,17 @@
+package com.edwise.pocs.springrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class ApplicationTest {
+
+    @Test
+    public void contextLoads()  {
+    }
+}
