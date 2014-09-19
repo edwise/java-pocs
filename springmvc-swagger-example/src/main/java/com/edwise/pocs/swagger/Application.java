7dc836b8bc93253d7b7ef175e2101796748399a6
eanton <eduardo.anton@medianet.es>
2014-09-19 12:59:34 +0200

    springmvc-swagger-example: añadida configuración swagger, swagger-ui, etc.

--- springmvc-swagger-example/src/main/java/com/edwise/pocs/swagger/Application.java
+++ springmvc-swagger-example/src/main/java/com/edwise/pocs/swagger/Application.java
@@ -2,6 +2,7 @@ package com.edwise.pocs.swagger;
 
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
+import com.mangofactory.swagger.plugin.EnableSwagger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@EnableSwagger
 public class Application {
     
     public static void main(String[] args) {
---
+++ springmvc-swagger-example/src/main/java/com/edwise/pocs/swagger/config/SwaggerSpringMvcConfig.java
@@ -0,0 +1,37 @@
+package com.edwise.pocs.swagger.config;
+
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+import com.wordnik.swagger.model.ApiInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerSpringMvcConfig {
+
+    private SpringSwaggerConfig springSwaggerConfig;
+
+    @Autowired
+    public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
+        this.springSwaggerConfig = springSwaggerConfig;
+    }
+
+    @Bean
+    public SwaggerSpringMvcPlugin customImplementation() {
+        return new SwaggerSpringMvcPlugin(this.springSwaggerConfig)
+                .apiInfo(apiInfo())
+                .includePatterns(".*/*.*");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Users API",
+                "Your user database!",
+                "http://userweb.userapi.com/Terms_of_service",
+                "userapi.manager@gmail.com",
+                "Apache 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0.html"
+        );
+    }
+}
