38aad2dc674de70bbd61aea19a1b9b39d607b690
eantonc <eantonc@gmail.com>
2014-10-11 20:19:37 +0200

    springbootseries-war: commit inicial.

---
+++ springboot-series-war/README.md
@@ -0,0 +1,1 @@
+Ejemplo Spring Boot, creado para el primer post de la serie Spring Boot: http://anotherdayanotherbug.wordpress.com/2014/ TODO
---
+++ springboot-series-war/src/main/java/com/edwise/springbootseries/war/controllers/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.edwise.springbootseries.war.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String sayHelloWorld() {
+        return "Hello World in your SpringBoot Application!";
+    }
+}
