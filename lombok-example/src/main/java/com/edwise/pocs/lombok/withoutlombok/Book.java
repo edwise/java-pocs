d3f1a0a4e7f4ec545d0d6d857b14d0cdfa83d8ab
eanton <eduardo.anton@medianet.es>
2014-10-27 15:51:53 +0100

    lombok-example: finished builder example.

--- lombok-example/src/main/java/com/edwise/pocs/lombok/withoutlombok/Book.java
+++ lombok-example/src/main/java/com/edwise/pocs/lombok/withoutlombok/Book.java
@@ -7,6 +7,13 @@ public class Book {
     private String isbn;
     private Short type;
 
+    public Book(Long id, String title, String isbn, Short type) {
+        this.id = id;
+        this.title = title;
+        this.isbn = isbn;
+        this.type = type;
+    }
+
     public Long getId() {
         return id;
     }
@@ -43,5 +50,48 @@ public class Book {
         return this;
     }
 
-    // TODO implement builder
+    public static BookBuilder builder() {
+         		return new BookBuilder();
+         	}
+
+         	public static class BookBuilder {
+                private Long id;
+                private String title;
+                private String isbn;
+                private Short type;
+
+         		private BookBuilder() {}
+
+         		public BookBuilder id(Long id) {
+         			this.id = id;
+         			return this;
+         		}
+
+         		public BookBuilder title(String title) {
+         			this.title = title;
+         			return this;
+         		}
+
+                public BookBuilder isbn(String isbn) {
+                    this.isbn = isbn;
+                    return this;
+                }
+
+                public BookBuilder type(Short type) {
+                    this.type = type;
+                    return this;
+                }
+
+         		@Override
+                public String toString() {
+         			return "BookBuilder(id = " + id
+                            + ", title = " + title
+                            + ", isbn = " + isbn
+                            + ", type = " + type + ")";
+         		}
+
+         		public Book build() {
+         			return new Book(id, title, isbn, type);
+         		}
+         	}
 }
