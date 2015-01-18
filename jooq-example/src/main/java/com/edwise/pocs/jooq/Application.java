2b86200c9a9f2a1b73c170a3976a735d6fe20e75
eantonc <eantonc@gmail.com>
2015-01-18 18:35:25 +0100

    jooq-example: implemented example.

--- jooq-example/src/main/java/com/edwise/pocs/jooq/Application.java
+++ jooq-example/src/main/java/com/edwise/pocs/jooq/Application.java
@@ -1,12 +1,5 @@
 package com.edwise.pocs.jooq;
 
-import com.edwise.pocs.jooq.jdbc.DBConnector;
-import org.jooq.DSLContext;
-import org.jooq.Record;
-import org.jooq.Result;
-import org.jooq.SQLDialect;
-import org.jooq.impl.DSL;
-import org.jooq.util.maven.generated.Tables;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,10 +7,6 @@ public class Application {
     private final static Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
-        DSLContext jooqDSL = DSL.using(DBConnector.getInstance().connection(), SQLDialect.H2);
-
-
-        Result<Record> result = jooqDSL.select().from(Tables.BOOK_CHARACTER).fetch();
-        log.info("Resultado query: \n{}", result.toString());
+        log.info("POC de jOOQ. Ejecuta los tests para probarla :)");
     }
 }
---
+++ jooq-example/src/test/java/com/edwise/pocs/jooq/JOOQExampleTest.java
@@ -0,0 +1,51 @@
+package com.edwise.pocs.jooq;
+
+import com.edwise.pocs.jooq.jdbc.DBConnector;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+import org.jooq.util.maven.generated.Tables;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JOOQExampleTest {
+    private final static Logger log = LoggerFactory.getLogger(JOOQExampleTest.class);
+
+    private DSLContext jooqDSL;
+
+    @Before
+    public void setUp() {
+        jooqDSL = DSL.using(DBConnector.getInstance().connection(), SQLDialect.H2);
+    }
+
+    @After
+    public void tearDown() {
+        DBConnector.getInstance().closeConnection();
+    }
+
+    @Test
+    public void testSelectAll() {
+        Result<Record> result = jooqDSL
+                .select()
+                .from(Tables.BOOK_CHARACTER)
+                .fetch();
+
+        log.info("Resultado query: \n{}", result.toString());
+    }
+
+    @Test
+    public void testStreamForEachJava8() {
+        jooqDSL.selectFrom(Tables.BOOK_CHARACTER)
+                .where(Tables.BOOK_CHARACTER.ID.in(2, 3))
+                .orderBy(Tables.BOOK_CHARACTER.NAME)
+                .fetch()
+                .stream()
+                .map(bookCharacter -> bookCharacter.getName().toUpperCase())
+                .forEach(s -> log.info("Name: {}", s));
+    }
+}
