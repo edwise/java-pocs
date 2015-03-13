76bb240448f2087329f6adf5351b18c7cbfb1c7a
eantonc <eantonc@gmail.com>
2015-03-13 18:20:19 +0100

    integrationtests-rest-example: added tests. Added jsonpath in pom.

--- integrationtests-rest-example/src/integration-test/java/com/edwise/pocs/integrationtestsrest/controller/InfoControllerIT.java
+++ integrationtests-rest-example/src/integration-test/java/com/edwise/pocs/integrationtestsrest/controller/InfoControllerIT.java
@@ -13,10 +13,14 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -40,13 +44,31 @@ public class InfoControllerIT {
     // TODO tests completos...
 
     @Test
-    public void getInfo_ShouldReturnCorrectInfo() throws Exception {
-        String jsonExpected = "{\"id\":1234,\"info\":\"Info 1234\",\"creationDateTime\":\"2001-12-12T13:40:30\"}";
+    public void getAll_InfosFound_ShouldReturnFoundInfos() throws Exception {
+        mockMvc.perform(get("/api/info/"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].id", is(120)))
+                .andExpect(jsonPath("$[0].info", is("Info 1234")))
+                .andExpect(jsonPath("$[0].creationDateTime", is(notNullValue())))
+                .andExpect(jsonPath("$[1].id", is(121)))
+                .andExpect(jsonPath("$[1].info", is("Info 4567")))
+                .andExpect(jsonPath("$[1].creationDateTime", is(notNullValue())))
+                .andExpect(jsonPath("$[2].id", is(122)))
+                .andExpect(jsonPath("$[2].info", is("Info 7892")))
+                .andExpect(jsonPath("$[2].creationDateTime", is(notNullValue())))
+        ;
+    }
 
+    @Test
+    public void getInfo_InfoFound_ShouldReturnCorrectInfo() throws Exception {
         mockMvc.perform(get("/api/info/{id}", INFO_ID_1234))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().string(jsonExpected));
+                .andExpect(jsonPath("$.id", is(1234)))
+                .andExpect(jsonPath("$.info", is("Info 1234")))
+                .andExpect(jsonPath("$.creationDateTime", is(notNullValue())));
     }
 
     @Test
