e586e78d9decb700dd18b433ab55b32bb589076b
eanton <eduardo.anton@medianet.es>
2015-04-28 17:05:56 +0200

    spring-rest-client-example: cleaned...

--- spring-rest-client-example/src/main/java/com/edwise/pocs/springrestclient/Application.java
+++ spring-rest-client-example/src/main/java/com/edwise/pocs/springrestclient/Application.java
@@ -1,6 +1,5 @@
 package com.edwise.pocs.springrestclient;
 
-import com.edwise.pocs.springrestclient.config.RestTemplateFactory;
 import com.edwise.pocs.springrestclient.model.Book;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -12,41 +11,28 @@ public class Application {
     private final static String URL_API_BOOKS = "http://private-114e-booksapi.apiary-mock.com/books";
 
     public static void main(String[] args) {
-        RestTemplate restTemplate = RestTemplateFactory.createRestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
 
         getOneBook(restTemplate);
         getAllBooks(restTemplate);
-
-        testArray();
-    }
-
-    private static void testArray() {
-        int[][] matrix = new int[10][10];
-        matrix[0][0] = 24;
-        matrix[0] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        matrix[1] = new int[]{1, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 10; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
     }
 
     private static void getOneBook(RestTemplate restTemplate) {
         ResponseEntity<Book> book = restTemplate.getForEntity(URL_API_BOOKS + "/{id}", Book.class, 12);
 
-        System.out.println("StatusCode = " + book.getStatusCode());
-        System.out.println("Headers = " + book.getHeaders());
-        System.out.println("Book getted: " + book.getBody());
+        System.out.println();
+        System.out.println("GET StatusCode = " + book.getStatusCode());
+        System.out.println("GET Headers = " + book.getHeaders());
+        System.out.println("GET Body (object): " + book.getBody());
     }
 
     private static void getAllBooks(RestTemplate restTemplate) {
         ResponseEntity<Book[]> books = restTemplate.getForEntity(URL_API_BOOKS, Book[].class);
 
-        System.out.println("StatusCode = " + books.getStatusCode());
-        System.out.println("Headers = " + books.getHeaders());
-        Arrays.asList(books.getBody()).forEach(System.out::println);
+        System.out.println();
+        System.out.println("GET All StatusCode = " + books.getStatusCode());
+        System.out.println("GET All Headers = " + books.getHeaders());
+        System.out.println("GET Body (object list): ");
+        Arrays.asList(books.getBody()).forEach(book -> System.out.println("--> " +book));
     }
 }
