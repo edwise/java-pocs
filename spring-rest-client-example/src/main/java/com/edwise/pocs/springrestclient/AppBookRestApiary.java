0b824400b945f98f9c796869cf2eedeecb1a8483
eantonc <eantonc@gmail.com>
2015-05-03 12:41:54 +0200

    spring-rest-client-example: some changes. Added post, put methods.

--- spring-rest-client-example/src/main/java/com/edwise/pocs/springrestclient/AppBookRestApiary.java
+++ spring-rest-client-example/src/main/java/com/edwise/pocs/springrestclient/AppBookRestApiary.java
@@ -15,31 +15,48 @@ public class AppBookRestApiary {
 
         getOneBook(restTemplate);
         getAllBooks(restTemplate);
+        postOneBook(restTemplate);
+        putOneBook(restTemplate);
         deleteBook(restTemplate);
     }
 
     private static void getOneBook(RestTemplate restTemplate) {
-        ResponseEntity<Book> book = restTemplate.getForEntity(URL_API_BOOKS + "{id}", Book.class, 12);
+        ResponseEntity<Book> response = restTemplate.getForEntity(URL_API_BOOKS + "{id}", Book.class, 12);
 
         System.out.println();
-        System.out.println("GET StatusCode = " + book.getStatusCode());
-        System.out.println("GET Headers = " + book.getHeaders());
-        System.out.println("GET Body (object): " + book.getBody());
+        System.out.println("GET StatusCode = " + response.getStatusCode());
+        System.out.println("GET Headers = " + response.getHeaders());
+        System.out.println("GET Body (object): " + response.getBody());
     }
 
     private static void getAllBooks(RestTemplate restTemplate) {
-        ResponseEntity<Book[]> books = restTemplate.getForEntity(URL_API_BOOKS, Book[].class);
+        ResponseEntity<Book[]> response = restTemplate.getForEntity(URL_API_BOOKS, Book[].class);
 
         System.out.println();
-        System.out.println("GET All StatusCode = " + books.getStatusCode());
-        System.out.println("GET All Headers = " + books.getHeaders());
+        System.out.println("GET All StatusCode = " + response.getStatusCode());
+        System.out.println("GET All Headers = " + response.getHeaders());
         System.out.println("GET Body (object list): ");
-        Arrays.asList(books.getBody()).forEach(book -> System.out.println("--> " + book));
+        Arrays.asList(response.getBody()).forEach(book -> System.out.println("--> " + book));
+    }
+
+    private static void postOneBook(RestTemplate restTemplate) {
+        // TODO
+
+        System.out.println();
+        System.out.println("POST executed");
+    }
+
+    private static void putOneBook(RestTemplate restTemplate) {
+        // TODO
+
+        System.out.println();
+        System.out.println("PUT executed");
     }
 
     private static void deleteBook(RestTemplate restTemplate) {
         restTemplate.delete(URL_API_BOOKS + "/{id}", 12);
 
         System.out.println();
+        System.out.println("DELETE executed");
     }
 }
--- spring-rest-client-example/src/main/java/com/edwise/pocs/springrestclient/AppInfoRestLocal.java
+++ spring-rest-client-example/src/main/java/com/edwise/pocs/springrestclient/AppInfoRestLocal.java
@@ -1,6 +1,7 @@
 package com.edwise.pocs.springrestclient;
 
 import com.edwise.pocs.springrestclient.model.Info;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,28 +16,48 @@ public class AppInfoRestLocal {
 
         getOneInfo(restTemplate);
         getAllInfos(restTemplate);
+        postOneInfo(restTemplate);
+        putOneInfo(restTemplate);
         deleteInfo(restTemplate);
     }
 
     private static void getOneInfo(RestTemplate restTemplate) {
-        ResponseEntity<Info> info = restTemplate.getForEntity(URL_API_INFO + "{id}", Info.class, 12);
+        ResponseEntity<Info> response = restTemplate.getForEntity(URL_API_INFO + "{id}", Info.class, 12);
 
         System.out.println();
         System.out.println("GET one executed");
-        System.out.println("GET StatusCode = " + info.getStatusCode());
-        System.out.println("GET Headers = " + info.getHeaders());
-        System.out.println("GET Body (object): " + info.getBody());
+        System.out.println("GET StatusCode = " + response.getStatusCode());
+        System.out.println("GET Headers = " + response.getHeaders());
+        System.out.println("GET Body (object): " + response.getBody());
     }
 
     private static void getAllInfos(RestTemplate restTemplate) {
-        ResponseEntity<Info[]> infos = restTemplate.getForEntity(URL_API_INFO, Info[].class);
+        ResponseEntity<Info[]> response = restTemplate.getForEntity(URL_API_INFO, Info[].class);
 
         System.out.println();
         System.out.println("GET All executed");
-        System.out.println("GET All StatusCode = " + infos.getStatusCode());
-        System.out.println("GET All Headers = " + infos.getHeaders());
+        System.out.println("GET All StatusCode = " + response.getStatusCode());
+        System.out.println("GET All Headers = " + response.getHeaders());
         System.out.println("GET Body (object list): ");
-        Arrays.asList(infos.getBody()).forEach(info -> System.out.println("--> " + info));
+        Arrays.asList(response.getBody()).forEach(info -> System.out.println("--> " + info));
+    }
+
+    private static void postOneInfo(RestTemplate restTemplate) {
+        // TODO
+        ResponseEntity<Info> response = restTemplate.postForEntity(URL_API_INFO, new Info(), Info.class);
+
+        System.out.println();
+        System.out.println("POST executed");
+        System.out.println("POST StatusCode = " + response.getStatusCode());
+        System.out.println("POST Headers = " + response.getHeaders());
+        System.out.println("POST Body (object): " + response.getBody());
+    }
+
+    private static void putOneInfo(RestTemplate restTemplate) {
+        // TODO
+
+        System.out.println();
+        System.out.println("PUT executed");
     }
 
     private static void deleteInfo(RestTemplate restTemplate) {
