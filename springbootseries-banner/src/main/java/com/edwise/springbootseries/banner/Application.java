59893acbdd7cb4d151a5c944a6ae11c70fbc67d5
eanton <eduardo.anton@medianet.es>
2015-07-16 17:01:10 +0200

    springbootseries-banner: first commit.

---
+++ springbootseries-banner/src/main/java/com/edwise/springbootseries/banner/Application.java
@@ -0,0 +1,14 @@
+package com.edwise.springbootseries.banner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        //app.setShowBanner(false);
+        app.run(args);
+    }
+}
---
+++ springbootseries-banner/src/test/java/com/edwise/springbootseries/banner/ApplicationTest.java
@@ -0,0 +1,10 @@
+package com.edwise.springbootseries.banner;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void testApplication() {
+    }
+}
