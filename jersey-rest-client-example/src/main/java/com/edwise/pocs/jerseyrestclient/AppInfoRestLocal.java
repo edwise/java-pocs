d5333c48c90405c27296d73845998556d389086f
eanton <eduardo.anton@medianet.es>
2015-09-08 13:10:37 +0200

    jersey-rest-client-example: resolved problem with jsr310 in local poc.

--- jersey-rest-client-example/src/main/java/com/edwise/pocs/jerseyrestclient/AppInfoRestLocal.java
+++ jersey-rest-client-example/src/main/java/com/edwise/pocs/jerseyrestclient/AppInfoRestLocal.java
@@ -2,16 +2,18 @@ package com.edwise.pocs.jerseyrestclient;
 
 import com.edwise.pocs.jerseyrestclient.model.Info;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import org.glassfish.jersey.client.ClientConfig;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 
 public class AppInfoRestLocal {
 
@@ -19,21 +21,18 @@ public class AppInfoRestLocal {
     private static final String URL_API = "http://localhost:8080/api/";
     private static final String INFO_RESOURCE = "info/";
 
-    private static ObjectMapper MAPPER;
-
     public static void main(String[] args) {
-        MAPPER = new ObjectMapper();
-        MAPPER.registerModule(new JSR310Module());
-        MAPPER.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
         Client client = ClientBuilder.newClient(
-                new ClientConfig().register(JacksonJsonProvider.class).register(MAPPER));
+                new ClientConfig().register(new JacksonJsonProvider(mapper)));
         WebTarget webTarget = client.target(URL_API).path(INFO_RESOURCE);
 
         getOneInfo(webTarget);
-//        getAllInfos(webTarget);
-//        postOneInfo(webTarget);
-//        putOneInfo(webTarget);
-//        deleteInfo(webTarget);
+        getAllInfos(webTarget);
+        postOneInfo(webTarget);
+        putOneInfo(webTarget);
+        deleteInfo(webTarget);
     }
 
     private static void getOneInfo(WebTarget webTarget) {
@@ -46,49 +45,52 @@ public class AppInfoRestLocal {
         System.out.println("GET Body (object): " + response.readEntity(Info.class));
     }
 
-//    private static void getAllInfos(WebTarget webTarget) {
-//        ResponseEntity<Info[]> response = webTarget.getForEntity(URL_API_INFO, Info[].class);
-//
-//        System.out.println();
-//        System.out.println("GET All executed");
-//        System.out.println("GET All StatusCode = " + response.getStatusCode());
-//        System.out.println("GET All Headers = " + response.getHeaders());
-//        System.out.println("GET Body (object list): ");
-//        Arrays.asList(response.getBody()).forEach(info -> System.out.println("--> " + info));
-//    }
-//
-//    private static void postOneInfo(WebTarget webTarget) {
-//        Info infoToInsert = createInfo(null, "UNew info text", LocalDateTime.of(2015, 5, 15, 21, 25, 30));
-//        ResponseEntity<Info> response = webTarget.postForEntity(URL_API_INFO, infoToInsert, Info.class);
-//
-//        System.out.println();
-//        System.out.println("POST executed");
-//        System.out.println("POST StatusCode = " + response.getStatusCode());
-//        System.out.println("POST Headers = " + response.getHeaders());
-//        System.out.println("POST Body (object): " + response.getBody());
-//    }
-//
-//    private static void putOneInfo(WebTarget webTarget) {
-//        Info infoToUpdate = createInfo(123L, "Updated text", LocalDateTime.of(2015, 5, 11, 22, 35, 35));
-//        webTarget.put(URL_API_INFO + "{id}", infoToUpdate, 123L);
-//
-//        System.out.println();
-//        System.out.println("PUT executed");
-//    }
-//
-//    private static void deleteInfo(WebTarget webTarget) {
-//        webTarget.delete(URL_API_INFO + "{id}", 12);
-//
-//        System.out.println();
-//        System.out.println("DELETE executed");
-//    }
-//
-//    private static Info createInfo(Long id, String infoText, LocalDateTime dateTime) {
-//        Info info = new Info();
-//        info.setId(id);
-//        info.setInfoText(infoText);
-//        info.setCreationDateTime(dateTime);
-//
-//        return info;
-//    }
+    private static void getAllInfos(WebTarget webTarget) {
+        Response response = webTarget.request(MediaType.APPLICATION_JSON_TYPE).get();
+
+        System.out.println();
+        System.out.println("GET All StatusCode = " + response.getStatus());
+        System.out.println("GET All Headers = " + response.getHeaders());
+        System.out.println("GET Body (object list): ");
+        Arrays.asList(response.readEntity(Info[].class))
+                .forEach(info -> System.out.println("--> " + info));
+    }
+
+    private static void postOneInfo(WebTarget webTarget) {
+        Info infoToInsert = createInfo(null, "New text", LocalDateTime.of(2015, 5, 15, 21, 25, 30));
+        Response response = webTarget.request()
+                .post(Entity.entity(infoToInsert, MediaType.APPLICATION_JSON_TYPE));
+
+        System.out.println();
+        System.out.println("POST executed");
+        System.out.println("POST StatusCode = " + response.getStatus());
+        System.out.println("POST Body (object): " + response.readEntity(Info.class));
+    }
+
+    private static void putOneInfo(WebTarget webTarget) {
+        Info infoToUpdate = createInfo(123L, "Info updated", LocalDateTime.of(2015, 5, 11, 22, 35, 35));
+        Response response = webTarget.path("123").request()
+                .put(Entity.entity(infoToUpdate, MediaType.APPLICATION_JSON_TYPE));
+
+        System.out.println();
+        System.out.println("PUT StatusCode = " + response.getStatus());
+        System.out.println("PUT Headers = " + response.getHeaders());
+    }
+
+    private static void deleteInfo(WebTarget webTarget) {
+        Response response = webTarget.path("122").request().delete();
+
+        System.out.println();
+        System.out.println("DELETE StatusCode = " + response.getStatus());
+        System.out.println("DELETE Headers = " + response.getHeaders());
+    }
+
+    private static Info createInfo(Long id, String infoText, LocalDateTime dateTime) {
+        Info info = new Info();
+        info.setId(id);
+        info.setInfoText(infoText);
+        info.setCreationDateTime(dateTime);
+
+        return info;
+    }
 }
