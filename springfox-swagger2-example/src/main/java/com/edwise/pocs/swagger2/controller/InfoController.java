4e4cd23b3b0596291f8c93f68f99c09cc796e66a
eanton <eduardo.anton@medianet.es>
2015-12-14 14:58:53 +0100

    springfox-swagger2-example: added swagger annotations in controller and entity.

--- springfox-swagger2-example/src/main/java/com/edwise/pocs/swagger2/controller/InfoController.java
+++ springfox-swagger2-example/src/main/java/com/edwise/pocs/swagger2/controller/InfoController.java
@@ -2,6 +2,11 @@ package com.edwise.pocs.swagger2.controller;
 
 import com.edwise.pocs.swagger2.entity.Info;
 import com.edwise.pocs.swagger2.service.InfoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -19,6 +24,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/infos")
+@Api(value = "infos", description = "Infos API", produces = "application/json")
 public class InfoController {
 
     @Autowired
@@ -27,6 +33,10 @@ public class InfoController {
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping(method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Get Infos", notes = "Returns all infos")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Exits one info at least")
+    })
     public List<Info> getAllInfos() {
         return infoService.findAll();
     }
@@ -34,12 +44,21 @@ public class InfoController {
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping(method = RequestMethod.GET, value = "{id}",
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public Info getInfo(@PathVariable Long id) {
+    @ApiOperation(value = "Get one Info", notes = "Returns one info")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Exists this info")
+    })
+    public Info getInfo(@ApiParam(defaultValue = "1", value = "The id of the info to return")
+                        @PathVariable Long id) {
         return infoService.findOne(id);
     }
 
     @RequestMapping(method = RequestMethod.POST,
             produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Create Info", notes = "Create a info")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Successful create of a info")
+    })
     public ResponseEntity<Info> createInfo(@RequestBody Info info) {
         Info infoCreated = infoService.save(info);
         return new ResponseEntity<>(createHeadersWithLocation(infoCreated), HttpStatus.CREATED);
@@ -47,13 +66,24 @@ public class InfoController {
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @RequestMapping(method = RequestMethod.PUT, value = "{id}")
-    public void updateInfo(@PathVariable Long id, @RequestBody Info info) {
+    @ApiOperation(value = "Update Info", notes = "Update a info")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Successful update of a info")
+    })
+    public void updateInfo(@ApiParam(defaultValue = "1", value = "The id of the info to update")
+                           @PathVariable Long id,
+                           @RequestBody Info info) {
         infoService.update(info.setId(id));
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @RequestMapping(method = RequestMethod.DELETE, value = "{id}")
-    public void deleteInfo(@PathVariable Long id) {
+    @ApiOperation(value = "Delete Info", notes = "Delete a info")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Successful delete of a info")
+    })
+    public void deleteInfo(@ApiParam(defaultValue = "1", value = "The id of the info to delete")
+                           @PathVariable Long id) {
         infoService.delete(id);
     }
 
--- springfox-swagger2-example/src/main/java/com/edwise/pocs/swagger2/entity/Info.java
+++ springfox-swagger2-example/src/main/java/com/edwise/pocs/swagger2/entity/Info.java
@@ -1,11 +1,20 @@
 package com.edwise.pocs.swagger2.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import java.time.LocalDateTime;
 
+@ApiModel(value = "Info entity", description = "Complete data of a entity Info")
 public class Info {
 
+    @ApiModelProperty(value = "The id of the info", required = false)
     private Long id;
+
+    @ApiModelProperty(value = "The text of the info", required = true)
     private String infoText;
+
+    @ApiModelProperty(value = "The date of the info", required = true)
     private LocalDateTime creationDateTime;
 
     public Long getId() {
