7889145c11abd9dfd47e25dd52153100a7589a56
eanton <eduardo.anton@medianet.es>
2015-12-14 12:30:11 +0100

    springfox-swagger2-example: added configuration class for Swagger2.

---
+++ springfox-swagger2-example/src/main/java/com/edwise/pocs/swagger2/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.edwise.pocs.swagger2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket newsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("api-infos")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(regex("/api.*"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Infos REST api")
+                .description("PoC of a REST api, Infos")
+                .termsOfServiceUrl("http://en.wikipedia.org/wiki/Terms_of_service")
+                .contact("edwise.null@gmail.com")
+                .license("Apache License Version 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("2.0")
+                .build();
+    }
+}
