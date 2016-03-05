7540a35f1e1080dc5ce57b982fbc73d2a403e4ab
eantonc <eantonc@gmail.com>
2016-03-05 12:36:04 +0100

    completable-future: project created, first commit.

---
+++ completablefuture-example/src/main/java/com/edwise/pocs/completablefuturepoc/Application.java
@@ -0,0 +1,8 @@
+package com.edwise.pocs.completablefuturepoc;
+
+public class Application {
+
+    public static void main(String[] args) {
+        // TODO implement
+    }
+}
---
+++ completablefuture-example/src/test/java/com/edwise/pocs/completablefuturepoc/ApplicationTest.java
@@ -0,0 +1,19 @@
+package com.edwise.pocs.completablefuturepoc;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApplicationTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testApplication() {
+        fail("Not yet implemented");
+    }
+}
