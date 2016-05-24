6b4e041c226b8a0a314494fb96193dd140a84758
eantonc <eantonc@gmail.com>
2016-05-24 20:49:54 +0200

    dropwizard-example: added test for resource.

---
+++ dropwizard-example/src/test/java/com/edwise/pocs/dropwizard/resource/MessageResourceTest.java
@@ -0,0 +1,17 @@
+package com.edwise.pocs.dropwizard.resource;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MessageResourceTest {
+    @Before
+    public void setUp() {
+        // TODO
+    }
+
+    @Test
+    public void sayHello() {
+        // TODO
+    }
+
+}
