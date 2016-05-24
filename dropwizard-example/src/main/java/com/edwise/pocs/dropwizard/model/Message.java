8bbdecc548dd8c188f17f83c699825c653b100f9
eantonc <eantonc@gmail.com>
2016-05-24 20:36:08 +0200

    dropwizard-example: added model.

---
+++ dropwizard-example/src/main/java/com/edwise/pocs/dropwizard/model/Message.java
@@ -0,0 +1,17 @@
+package com.edwise.pocs.dropwizard.model;
+
+public class Message {
+
+    private String message;
+
+    public Message() {
+    }
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
