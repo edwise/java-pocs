2170145031150c9d7d1073734de0e61d3c33f7f5
eantonc <eantonc@gmail.com>
2016-06-11 16:53:39 +0200

    java8-function-methods-example: first commit.

---
+++ java8-function-methods-example/src/main/java/com/edwise/pocs/java8functionmethods/Application.java
@@ -0,0 +1,8 @@
+package com.edwise.pocs.java8functionmethods;
+
+public class Application {
+
+    public static void main(String[] args) {
+        // TODO implement
+    }
+}
---
+++ java8-function-methods-example/src/test/java/com/edwise/pocs/java8functionmethods/ApplicationTest.java
@@ -0,0 +1,19 @@
+package com.edwise.pocs.java8functionmethods;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApplicationTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testApplication() {
+        fail("Not yet implemented");
+    }
+}
