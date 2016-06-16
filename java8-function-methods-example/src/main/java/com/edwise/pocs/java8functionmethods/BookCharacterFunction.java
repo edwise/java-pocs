6c38334e7dba0e6e319845df408859cc05291e7b
eantonc <eantonc@gmail.com>
2016-06-16 20:44:39 +0200

    java8-function-methods-example: added model and examples.

---
+++ java8-function-methods-example/src/main/java/com/edwise/pocs/java8functionmethods/BookCharacterFunction.java
@@ -0,0 +1,23 @@
+package com.edwise.pocs.java8functionmethods;
+
+import com.edwise.pocs.java8functionmethods.model.BookCharacter;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class BookCharacterFunction {
+
+    public static Function<List<BookCharacter>, BookCharacter> findFirstSwordsman() {
+        return list ->
+                list.stream()
+                    .filter(bookChar -> BookCharacter.Weapon.SWORD.equals(bookChar.getMainWeapon()))
+                    .findFirst()
+                    .orElse(new BookCharacter("NOBODY", 0, BookCharacter.Weapon.SWORD, true));
+    }
+
+    public static Function<BookCharacter, String> characterToCode() {
+        return bookCharacter ->
+                bookCharacter.getName() + "::" + bookCharacter.getMainWeapon() + "::" +
+                        (bookCharacter.isHuman() ? "human" : "nonHuman");
+    }
+}
--- java8-function-methods-example/src/test/java/com/edwise/pocs/java8functionmethods/FunctionMethodsTest.java
+++ java8-function-methods-example/src/test/java/com/edwise/pocs/java8functionmethods/FunctionMethodsTest.java
@@ -7,6 +7,14 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static com.edwise.pocs.java8functionmethods.BookCharacterFunction.characterToCode;
+import static com.edwise.pocs.java8functionmethods.BookCharacterFunction.findFirstSwordsman;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
 
 public class FunctionMethodsTest {
 
@@ -26,6 +34,49 @@ public class FunctionMethodsTest {
     }
 
     @Test
-    public void testFunctionMethod() {
+    public void testFunctionAndThenMethod() {
+        Function<List<BookCharacter>, String> andThenFunction =
+                findFirstSwordsman().andThen(characterToCode());
+
+        assertThat(andThenFunction.apply(bookCharacters))
+                .isEqualTo("Aragorn::SWORD::human");
+    }
+
+    @Test
+    public void testFunctionComposeMethod() {
+        Function<List<BookCharacter>, String> composeFunction =
+                characterToCode().compose(findFirstSwordsman());
+
+        assertThat(composeFunction.apply(bookCharacters))
+                .isEqualTo("Aragorn::SWORD::human");
+    }
+
+    @Test
+    public void testWithNoFunctionIdentityMethod() {
+        Map<String, BookCharacter> characterMap =
+                bookCharacters.stream()
+                              .collect(Collectors.toMap(BookCharacter::getName, bookChar -> bookChar));
+
+        assertThatMapContainsListCharacters(characterMap);
+    }
+
+    @Test
+    public void testWithFunctionIdentityMethod() {
+        Map<String, BookCharacter> characterMap =
+                bookCharacters.stream()
+                              .collect(Collectors.toMap(BookCharacter::getName, Function.identity()));
+
+        assertThatMapContainsListCharacters(characterMap);
+    }
+
+    private void assertThatMapContainsListCharacters(Map<String, BookCharacter> characterMap) {
+        assertThat(characterMap).hasSize(bookCharacters.size())
+                                .containsOnly(entry("Gandalf", bookCharacters.get(0)),
+                                        entry("Aragorn", bookCharacters.get(1)),
+                                        entry("Gimli", bookCharacters.get(2)),
+                                        entry("Legolas", bookCharacters.get(3)),
+                                        entry("Boromir", bookCharacters.get(4)),
+                                        entry("Frodo", bookCharacters.get(5)),
+                                        entry("Sam", bookCharacters.get(6)));
     }
 }
